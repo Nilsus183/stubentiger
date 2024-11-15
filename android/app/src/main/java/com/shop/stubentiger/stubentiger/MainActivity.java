b561bb8f36e56a9e638f64041affd835d5aa93cc
Nils <nilsstolle.privat@gmail.com>
2024-11-15 23:46:56 +0100

    v1

---
+++ android/app/src/main/java/com/shop/stubentiger/stubentiger/MainActivity.java
@@ -0,0 +1,6 @@
+package com.shop.stubentiger.stubentiger;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Farbe für Status- und Navigationsleiste festlegen
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      statusBarColor: Color(0xFFFDFDFD),
+      systemNavigationBarColor: Color(0xFFFDFDFD),
+      systemNavigationBarIconBrightness: Brightness.dark,
+    ));
+
+    return MaterialApp(
+      home: WebViewExample(),
+    );
+  }
+}
+
+class WebViewExample extends StatefulWidget {
+  @override
+  _WebViewExampleState createState() => _WebViewExampleState();
+}
+
+class _WebViewExampleState extends State<WebViewExample> {
+  late final WebViewController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..loadRequest(Uri.parse('https://www.stubentiger24.com'));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: WebViewWidget(controller: _controller),
+      ),
+    );
+  }
+}
